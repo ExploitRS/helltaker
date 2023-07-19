df6305d2110ac5f985911248b7a87bc2562ac17f
John Paul <nullopt_t@proton.me>
2023-07-20 01:16:26 +0900

    Separate maze and tilemap implementations

--- src/helltaker/maze.py
+++ src/helltaker/maze.py
@@ -5,9 +5,24 @@ import pyxel
 import conf
 
 class Maze:
-    def __init__(self, conf: conf.Conf, tm, u, v, w, h, col):
-        self._x_   = conf._x_
-        self._y_   = conf._y_
+    def __init__(self, tm: TileMap, start_pos,end_pos, enemies):
+        self._tilemap_ = tm
+        self._is_clear = False
+        self._start_pos_ = start_pos
+        self._end_pos = end_pos
+        self._enemy_pos_list_ = enemy_pos_list
+ 
+    def update(self):
+        pass
+
+    def draw(self):
+        pyxel.bltm((self._x_ / 2) - (self._w_ / 2), (self._y_ / 2) - (self._h_ / 2), self._tm_, self._u_, self._v_, self._w_, self._h_)
+
+# For pyxel purpose
+class TileMap:
+    def __init__(self, x, y, tm, u, v, w, h, col):
+        self._x_   = x
+        self._y_   = y
         self._tm_  = tm
         self._u_   = u
         self._v_   = v
@@ -15,11 +30,6 @@ class Maze:
         self._h_   = h
         self._col_ = col
 
-    def update(self):
-        pass
-
-    def draw(self):
-        pyxel.bltm((self._x_ / 2) - 28, (self._y_ / 2) - 24, self._tm_, self._u_, self._v_, self._w_, self._h_)
 
 class Tile:
     def __init__(self, x, y, tile_kind):
