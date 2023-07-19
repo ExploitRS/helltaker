f664f726cd0bf6758da4863dd947c87d0a49b932
John Paul <nullopt_t@proton.me>
2023-07-20 00:31:38 +0900

    Changed key event to be retrieved

--- src/helltaker/player.py
+++ src/helltaker/player.py
@@ -8,17 +8,17 @@ class Player:
         self._direction_ = None
 
     def update(self):
-        if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_LEFT) or pyxel.btn(pyxel.KEY_A):
-            self._x_ = max(self._x_ - 2, 0)
+        if pyxel.btnp(pyxel.KEY_LEFT) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_LEFT) or pyxel.btnp(pyxel.KEY_A):
+            self._x_ = max(self._x_ - 8, 0)
 
-        if pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT) or pyxel.btn(pyxel.KEY_D):
-            self._x_ = min(self._x_ + 2, pyxel.width -16)
+        if pyxel.btnp(pyxel.KEY_RIGHT) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT) or pyxel.btnp(pyxel.KEY_D):
+            self._x_ = min(self._x_ + 8, pyxel.width -16)
 
-        if pyxel.btn(pyxel.KEY_UP) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_UP) or pyxel.btn(pyxel.KEY_W):
-            self._y_ = self._y_ - 2
+        if pyxel.btnp(pyxel.KEY_UP) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_UP) or pyxel.btnp(pyxel.KEY_W):
+            self._y_ = self._y_ - 8
 
-        if pyxel.btn(pyxel.KEY_DOWN) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_DOWN) or pyxel.btn(pyxel.KEY_S):
-            self._y_ = self._y_ + 2
+        if pyxel.btnp(pyxel.KEY_DOWN) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_DOWN) or pyxel.btnp(pyxel.KEY_S):
+            self._y_ = self._y_ + 8
 
     def draw(self):
         pyxel.blt(
