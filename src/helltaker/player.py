853c91761b8519ada3c45575bcdcf708b1305967
John Paul <nullopt_t@proton.me>
2023-07-21 05:16:40 +0900

    Update player module

--- src/helltaker/player.py
+++ src/helltaker/player.py
@@ -17,16 +17,9 @@ class Player(Avatar):
 
     def neighbor(self, dir: Direction) -> Position:
         pos = deepcopy(self._pos_)
-        print(f"x: {pos.x}")
-        print(f"y: {pos.y}")
         dir_pos = dir.value
-        print(f"dirx: {dir_pos.x}")
         pos.sub_update(dir_pos.x, dir_pos.y)
-        print(f"neix {pos.x}")
-        print(f"neiy {pos.y}")
-        print(f"naita: {self._pos_.x}")
         return pos
 
     def move(self, pos: Position):
-        print(f"move: {pos.x}")
         self._pos_ = pos
