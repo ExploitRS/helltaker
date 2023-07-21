9ac2068c913d940b049dab7f9900593fec2be2e9
John Paul <nullopt_t@proton.me>
2023-07-22 06:13:18 +0900

    rendering of enemies

--- src/helltaker/enemy.py
+++ src/helltaker/enemy.py
@@ -1,8 +1,13 @@
 import pyxel
 
-from avatar import Avatar
+from avatar import Avatar, Position
 
 class Enemy(Avatar):
-    def __init__(self):
-        # self.
-        pass
+    def __init__(self, x, y, img, u, v, w, h, col, damage):
+        super().__init__(x, y, img, u, v, w, h, col)
+        self._damage_ = damage
+
+class Wizard(Enemy):
+    def __init__(self, x, y):
+        DAMAGE = 1
+        super().__init__(x, y, 0, 64, 8, 8, 8, 0, DAMAGE)
