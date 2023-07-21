172648669245b7ca971a90c407c76bbe223014e9
John Paul <nullopt_t@proton.me>
2023-07-22 04:23:11 +0900

    Command line arguments now accepted

--- src/helltaker/main.py
+++ src/helltaker/main.py
@@ -1,9 +1,23 @@
+import argparse
+
 import pyxel
 
 import game
 
 def main():
-    game.App()
+    parser = argparse.ArgumentParser(
+        "Helltaker",
+        description="An implementation of Helltaker in Python"
+    )
+    parser.add_argument("-d", "--debug", action="store_true")
+
+    debug = False
+    args = parser.parse_args()
+
+    if args.debug:
+        debug = True
+
+    game.App(debug)
 
 if __name__ == "__main__":
     main()
