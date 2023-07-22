fd701b2c0950458650062fe1661677dbaad84eea
John Paul <nullopt_t@proton.me>
2023-07-23 08:31:44 +0900

    Further abstracted enemy

--- src/helltaker/enemy.py
+++ src/helltaker/enemy.py
@@ -1,21 +1,10 @@
 import pyxel
 
 from avatar import Avatar, Position
+from obstract import Obstract
 
-class Enemy(Avatar):
-    def __init__(self, x, y, img, u, v, w, h, col, damage, is_alive = True):
-        super().__init__(x, y, img, u, v, w, h, col)
-        self._damage_ = damage
-        self.default_alive = is_alive
-        self._is_alive_ = is_alive
-
-    def respawn(self):
-        self.reset_pos()
-        self._is_alive_ = self.default_alive
-
-    def render(self):
-        if self._is_alive_:
-            self.draw()
+class Enemy(Obstract):
+    pass
 
 class Wizard(Enemy):
     def __init__(self, x, y):
