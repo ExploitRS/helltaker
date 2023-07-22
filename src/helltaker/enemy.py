8868aa93e50f47426735415f26f74cfeadfd2671
John Paul <nullopt_t@proton.me>
2023-07-23 06:07:43 +0900

    Added life status check and respawn method to Enemy

--- src/helltaker/enemy.py
+++ src/helltaker/enemy.py
@@ -3,9 +3,15 @@ import pyxel
 from avatar import Avatar, Position
 
 class Enemy(Avatar):
-    def __init__(self, x, y, img, u, v, w, h, col, damage):
+    def __init__(self, x, y, img, u, v, w, h, col, damage, is_alive = True):
         super().__init__(x, y, img, u, v, w, h, col)
         self._damage_ = damage
+        self.default_alive = is_alive
+        self._is_alive_ = is_alive
+
+    def rspawn(self):
+        self.reset_pos()
+        self._is_alive_ = self.default_alive
 
 class Wizard(Enemy):
     def __init__(self, x, y):
