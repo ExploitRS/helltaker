20a53cf6a59b743fa6807a6cf56463e67adc5716
John Paul <nullopt_t@proton.me>
2023-07-23 10:36:00 +0900

    Implemented a method  to return positions in all directions

--- src/helltaker/hellgirl.py
+++ src/helltaker/hellgirl.py
@@ -1,7 +1,14 @@
 import pyxel
 
-from avatar import Avatar
+from avatar import Avatar, Position, Direction
 
 class Portal(Avatar):
     def __init__(self, x, y):
         super().__init__(x, y, 0, 16, 32, 8, 8, 0)
+
+    def neighbor_vec(self) -> list[Position]:
+        p = self._pos_
+        return [
+            p + Direction.Left.value, p + Direction.Right.value,
+            p + Direction.Up.value, p + Direction.Down.value
+        ]
